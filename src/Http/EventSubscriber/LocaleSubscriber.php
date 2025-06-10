e802cd7d86f5c2523164aed82175e54a77a266e9
INTERNATIONALES WEB SERVICES <franckagbokoudjo301@gmail.com>
2025-06-10 22:00:59 +0100

    Ajouter .env à .gitignore

--- src/Http/EventSubscriber/LocaleSubscriber.php
+++ src/Http/EventSubscriber/LocaleSubscriber.php
@@ -30,7 +30,6 @@ class LocaleSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            // must be registered before (i.e. with a higher priority than) the default Locale listener
             KernelEvents::REQUEST => [['onKernelRequest', 20]],
         ];
     }
