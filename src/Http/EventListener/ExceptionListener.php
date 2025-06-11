eb6ba7d0413489dd7e7188d3ee94cbe95808911e
INTERNATIONALES WEB SERVICES <franckagbokoudjo301@gmail.com>
2025-06-11 18:46:49 +0100

    Mise à jour des fichiers sitemap finale

--- src/Http/EventListener/ExceptionListener.php
+++ src/Http/EventListener/ExceptionListener.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of the project by AGBOKOUDJO Franck.
+ *
+ * (c) AGBOKOUDJO Franck <franckagbokoudjo301@gmail.com>
+ * Phone: +229 0167 25 18 86
+ * LinkedIn: https://www.linkedin.com/in/internationales-web-services-120520193/
+ * Github: https://github.com/Agbokoudjo/norldfinance.com
+ * Company: INTERNATIONALES WEB SERVICES
+ *
+ * For more information, please feel free to contact the author.
+ */
 
 namespace App\Http\EventListener;
 
@@ -10,6 +21,10 @@ use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\Routing\RouterInterface;
 
+/**
+ * @author AGBOKOUDJO Franck <franckagbokoudjo301@gmail.com>
+ * @package <https://github.com/Agbokoudjo/norldfinance.com>
+ */
 #[AsEventListener(event: ExceptionEvent::class, method: "onExceptionEvent")]
 final class ExceptionListener
 {
